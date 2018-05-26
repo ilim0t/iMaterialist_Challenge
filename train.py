bf4c0025077f65b1c8d8e37634689dbb0bc150a1
ilim <ilim0t14@gmail.com>
2018-05-26 21:07:32 +0900

    bug fix

--- train.py
+++ train.py
@@ -12,7 +12,6 @@ warnings.filterwarnings('ignore', category=RuntimeWarning, message="More than 20
 import chainer
 import chainer.functions as F
 import chainer.links as L
-from chainer.datasets import TransformDataset
 from chainer import training
 from chainer.training import extensions
 
@@ -20,12 +19,10 @@ import models.model
 
 import numpy as np
 import argparse
-
 import pickle
 import os
 import json
 from PIL import Image
-
 import matplotlib as mpl
 import platform
 
@@ -37,20 +34,16 @@ import matplotlib.pyplot as plt
 from scipy.ndimage.interpolation import rotate
 from scipy.misc import imresize
 
-import chainer.training.extensions.evaluator
-
 import shutil
-
 import copy
 import six
-from chainer.dataset import convert
-from chainer import reporter as reporter_module
 
 
 class MyEvaluator(extensions.Evaluator):
     """
     chainer標準のEvaliator(testデータの評価出力)で
-    取るべきでないのに各バッチの平均が取られてしまう問題を修正したクラス
+    取るべきでないのに各バッチの平均が取られてしまう問題を修正し
+    省略したPrintReportに対応したクラス
     """
     def evaluate(self):
         iterator = self._iterators['main']
@@ -65,12 +58,12 @@ class MyEvaluator(extensions.Evaluator):
         else:
             it = copy.copy(iterator)
 
-        summary = reporter_module.DictSummary()
+        summary = chainer.reporter.DictSummary()
         freq_errs = dict()
 
         for batch in it:
             observation = {}
-            with reporter_module.report_scope(observation):
+            with chainer.reporter.report_scope(observation):
                 in_arrays = self.converter(batch, self.device)
                 with chainer.function.no_backprop_mode():
                     if isinstance(in_arrays, tuple):
@@ -90,43 +83,47 @@ class MyEvaluator(extensions.Evaluator):
 
 
 class Transform(object):
-    def __init__(self, args, isTrain=True):
+    def __init__(self, args, photo_nums, isTrain=True):
         self.label_variety = args.label_variety
         self.size = args.size
         self.data_folder = 'data/' + args.object + '_images/'
 
         with open('input/train.json', 'r') as f:  # 教師データの読み込み
             self.json_data = [[int(j) for j in i["labelId"]] for i in
-                              json.load(f)["annotations"][:args.total_photo_num]]
+                              json.load(f)["annotations"][:photo_nums[-1]]]
 
     def __call__(self, num):
         img_data = Image.open(self.data_folder + str(num) + '.jpg')
-
         array_img = np.asarray(img_data)
-        if np.random.rand < 0.5:
+
+        # 各種 augmentation
+        if np.random.rand() < 0.5:
             array_img = self.rotation(array_img)
-        if array_img[:2] != [self.size]:
+        if (array_img[:2] != [self.size]).any():
             array_img = imresize(array_img, [self.size] * 2)
         array_img = self.horizontal_flip(array_img)
+
         array_img = self.zscore(array_img)
         array_img = array_img.transpose(2, 0, 1).astype(np.float32)
 
-        one_hot_label = np.array([1 if i in self.json_data[num] else 0 for i in range(1, self.label_variety + 1)])
+        label = np.array([1 if i in self.json_data[num - 1] else 0 for i in range(1, self.label_variety + 1)])
         # すべてのlabel番号に対しlebelがついているならば1,そうでないならば0を入れたリスト
         # 例: 1, 2, 10 のラベルがついている場合
         # [1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, ...]
-        return array_img, one_hot_label
+        return array_img, label
 
     def horizontal_flip(self, img):
+        # 左右反転
         return img[:, ::-1, :]
 
     def rotation(self, img):
+        # 回転
         angle = np.random.randint(-20, 20)
         img = rotate(img, angle)
-        img = imresize(img, [self.size] * 2)
         return img
 
     def zscore(self, img):
+        # 正規化
         xmean = np.mean(img)  # 110~170
         xstd = np.std(img)  # 80~110
         img = (img - xmean) / xstd
@@ -137,21 +134,24 @@ def photos(args):
     # 存在するファイルから写真のみ列挙してself.file_numsに格納
     data_folder = 'data/' + args.object + '_images/'
     photo_nums = os.listdir(data_folder)
-    for i in ['.gitkeep', '.DS_Store', 'trash']:
+    for i in ['.gitkeep', '.DS_Store', 'trash']:  # 画像ではないファイル,ディレクトリを除外
         if i in photo_nums:
             photo_nums.remove(i)
     photo_nums = [int(i.split('.')[0]) for i in photo_nums]
     photo_nums.sort()
-    if args.cleanup:
+
+    if args.cleanup:  # 指定された場合 真っ白なファイルなどを除外する
         removed = []
         for i in photo_nums:
-            if os.path.getsize(data_folder + str(i) + '.jpg') in [874, 1854, 2588, 5106, 11197, 12814, 6305]:  # [4019, 16161, 35, 1485, 7742, 34262, 5098] 40000枚中これらのみ発見
+            # [4019, 16161, 35, 1485, 7742, 34262, 5098] 40000枚中これらのみ発見
+            if os.path.getsize(data_folder + str(i) + '.jpg') in [874, 1854, 2588, 5106, 11197, 12814, 6305]:
                 photo_nums.remove(i)
                 removed.append(i)
                 shutil.move(data_folder + str(i) + '.jpg', data_folder + 'trash/' + str(i) + '.jpg')
-        with open('removed.txt', 'w') as f:
+        with open('removed.txt', 'wb') as f:
             pickle.dump(removed, f)
-    if args.total_photo_num != -1:
+
+    if args.total_photo_num != -1:  # 上限が指定されている場合それに合わせる
         photo_nums = photo_nums[:args.total_photo_num]
     return photo_nums
 
@@ -191,6 +191,7 @@ def main():
 
     # モデルの定義
     model = models.model.ResNet(args.label_variety)
+    #model = models.model.Mymodel(args.label_variety)
 
     # GPUで動かせるのならば動かす
     if args.gpu >= 0:
@@ -205,8 +206,8 @@ def main():
     photo_nums = photos(args)
     train, test = chainer.datasets.split_dataset_random(photo_nums,
                                                         int(len(photo_nums) * 0.8), seed=0)  # 2割をvalidation用にとっておく
-    train = chainer.datasets.TransformDataset(train, Transform(args, True))
-    test = chainer.datasets.TransformDataset(test, Transform(args, False))
+    train = chainer.datasets.TransformDataset(train, Transform(args, photo_nums, True))
+    test = chainer.datasets.TransformDataset(test, Transform(args, photo_nums, False))
 
     train_iter = chainer.iterators.SerialIterator(train, args.batchsize)
     test_iter = chainer.iterators.SerialIterator(test, args.batchsize,
@@ -265,10 +266,14 @@ def main():
     # 各データでの評価の表示(欄に関する)設定
     trainer.extend(extensions.PrintReport(
         ['epoch', 'iteration', 'main/loss', 'val/loss',
-         'main/acc', 'vali/acc',
-         'main/freq_err', 'val/freq_err', 'main/acc_66',
-         'main/acc2', 'val/acc2', 'main/f1', 'val/f1', 'elapsed_time'
-         ]))
+         'main/acc', 'val/acc', 'main/f1', 'val/f1', 'elapsed_time']))
+    # trainer.extend(extensions.PrintReport(
+    #     ['epoch', 'iteration', 'main/loss', 'val/loss',
+    #      'main/acc', 'val/acc', 'main/acc2', 'val/acc2',
+    #      'main/acc_66', 'main/f1', 'val/f1',
+    #      'main/freq_err', 'val/freq_err', 'elapsed_time'
+    #      ]))
+
 
     # プログレスバー表示の設定
     trainer.extend(extensions.ProgressBar(update_interval=args.interval))
